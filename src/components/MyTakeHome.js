5a923adf0c2ab74091788dea7d5c48d3b3b1e80d
saeed-wahab <salamwahab16@gmail.com>
2021-09-04 18:04:31 +0000

    net salary layout

---
+++ src/components/MyTakeHome.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MyTakeHome(){
+    return(
+        <div className="modal is-active">
+            <div className="modal-background"></div>
+            <div className="modal-content">
+                <div className="box">
+                Hello, I am you Box model
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close"></button>
+        </div>
+
+    )
+}
+
+export default MyTakeHome;
