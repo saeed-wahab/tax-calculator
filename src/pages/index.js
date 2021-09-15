4b2650c09c234e2f879997955c4cce680d9bb897
saeed-wahab <salamwahab16@gmail.com>
2021-09-15 14:17:35 +0000

    minor ui update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { useStore } from "../global-state";
 import Form from "../components/Form";
 import { selectSsnit, selectTax,selectIncome, selectTaxTable } from "../global-state/selectors";
@@ -10,18 +10,28 @@ function LandingPage(){
     const ssnit  = selectSsnit(state);
     const income = selectIncome(state)
     const taxTable= selectTaxTable(state);
+    const [tableVisibility, setVisibility] = useState(false);
+
+    
     return(
         <div className="box">
             <h1 className="title">TAX Calculator(<abbr title="Pay As you Earn">PAYE</abbr>)</h1>
-           <Form /><br/>
-           <ul>
+           <Form /><br/><hr/>
+           <ul className="deduct-list">
                <li>SSNIT: {ssnit}</li>
                <li>Total Tax(to be paid) : {tax}</li>
                <li>Net Salary: {income}</li>
-              
-
-           </ul>
-           <Table list={taxTable} tax={tax}/>
+            </ul><hr/>
+           {taxTable.length ? 
+                (
+                <button 
+                    onClick ={(e)=> setVisibility(!tableVisibility)}
+                    className="button is-primary">
+                    {tableVisibility ?"Hide Break Down" : "Show Tax Break Down"}
+                </button>
+                )
+                : null }
+           {tableVisibility ? (<Table list={taxTable} tax={tax} />) : null }
         </div>
     )
 }
